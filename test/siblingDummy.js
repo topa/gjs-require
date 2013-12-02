a59fc213f1700672ff27b9c7790f9bdfa118b75e
Paul Torka <paul.torka@peerigon.com>
2013-12-02 22:34:57 +0100

    - Added some more tests, dummies and also some todos. - Added .idea-Folder to gitignore

---
+++ test/siblingDummy.js
@@ -0,0 +1,4 @@
+
+function whereAreYou() {
+    return "sibling";
+}
--- test/testRequire.js
+++ test/testRequire.js
@@ -1,9 +1,6 @@
 imports.require;
 const JSUnit = require("jsUnit");
 
-function setUp() {
-
-}
 
 function testSanity() {
     JSUnit.assertTrue("It should be a function", typeof JSUnit.gjstestRun == "function");
@@ -21,8 +18,72 @@ function testPwd() {
     JSUnit.assertEquals("It should be 'gjs-require/test'", index, expectedIndex);
 }
 
-function tearDown() {
+// @TODO
+//function testRequireSibling() {
+//    let origin = require("./siblingDummy").whereAreYou();
+//    JSUnit.assertEquals("It should import siblings", origin, "sibling");
+//}
 
+// @TODO
+//function testRequireInChildFolder() {
+//    let origin = require("./childfolder/childFolderDummy").whereAreYou();
+//    JSUnit.assertEquals("It should import siblings", origin, "child");
+//}
+
+// @TODO
+//function testRequireInParentFolder() {
+//    let origin = require("../parentDummy").whereAreYou();
+//    JSUnit.assertEquals("It should import siblings", origin, "parent");
+//}
+
+function testRequireLang() {
+    JSUnit.assertNotUndefined("It should import lang", require("lang").Class);
 }
 
-JSUnit.gjstestRun(this, setUp, tearDown);
+function testRequireMainloop() {
+    JSUnit.assertTrue("If should import mainloop", typeof require("mainloop").timeout_add == "function");
+}
+
+// @TODO
+//function testRequireGi() {
+//    JSUnit.assertNotUndefined("It should import gi", require("gi").Soup);
+//    JSUnit.assertNotUndefined("It should import gi", require("gi").Gtk);
+//}
+
+function testRequireGiGObject() {
+    JSUnit.assertNotUndefined("It should import gi.GObject", require("gi/GObject").Class);
+}
+
+function testRequireGiGio() {
+    JSUnit.assertNotUndefined("It should import gi.Gio", require("gi/Gio"));
+    JSUnit.assertTrue(
+        "It should import go.Gio.SettingsSchemaSource",
+        typeof require("gi/Gio/SettingsSchemaSource").get_default == "function"
+    );
+}
+
+function testRequireGiGtk() {
+    JSUnit.assertNotUndefined("It should import gi.Gtk", require("gi/Gtk").IconTheme);
+    JSUnit.assertTrue(
+        "It should import gi.Gtk.IconTheme.get_default()",
+        typeof require("gi/Gtk/IconTheme").get_default == "function"
+    );
+}
+
+// @TODO
+//function testRequireGiSt() {
+//    JSUnit.assertNotUndefined("It should import gi.St", require("gi/St").Side.Top);
+//}
+
+function testRequireMisc() {
+//    JSUnit.assertNotUndefined("It should import misc", require("misc"));
+    JSUnit.assertTrue("It should import misc.params.parse", typeof require("misc/params/parse") == "function");
+    JSUnit.assertTrue("It should also import misc.params.parse", typeof require("misc/params").parse == "function");
+    // @TODO
+//    JSUnit.assertTrue(
+//        "It should import misc.extensionUtils",
+//        typeof require("misc/extensionUtils/getCurrentExtension") == "function"
+//    )
+}
+
+JSUnit.gjstestRun(this, JSUnit.setUp, JSUnit.tearDown);
