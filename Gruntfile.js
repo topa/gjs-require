8ecdde55c4ae64270f60ddeb641946de2c813aa0
Paul Torka <paul.torka@peerigon.com>
2013-12-02 21:48:24 +0100

    Added grunt and tasks to watch on changes and (re)-run tests.

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+"use strict"
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        shell: {
+            runTests: {
+                command: "gjs-console test/testRequire.js -I ./",
+                options: { stdout: true, stderr: true }
+            }
+        },
+        watch: {
+            test: {
+                files: ["./require.js", "./test/*.js"],
+                tasks: ["shell:runTests"],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-shell");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.registerTask("watch-test", "watch:test");
+
+};
